36bddb27ec99afe5ad9aea5659398d53f71f8ea1
Snowrries <Snowrries@users.noreply.github.com>
2015-04-29 01:37:05 -0400

    Create comb.c

---
+++ comb.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+/*argv[0] is the Circuit Description File
+  argv[1] is the Input Values File
+  */
+  
+  
+int main(int argc, char* argv[]){
+	if(argc != 3){
+		printf("Incorrect number of arguments.");
+	}
+	
+	
+	
+	
+	
+}
