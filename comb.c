2609d7d4eada5999ade01fd7aae6d5fd0837a803
Snowrries <Snowrries@users.noreply.github.com>
2015-05-04 19:45:35 -0400

    Update comb.c

--- comb.c
+++ comb.c
@@ -3,41 +3,48 @@
 /*argv[0] is the Circuit Description File
   argv[1] is the Input Values File
   */
+	typedef *struct entry_t{
+		char name;
+		int value;
+	}entry;
+	
+
   	char *buffer;
-	char *inputs;
-	char *outputs;
-	int *invals;
-	int *outvals;
+	//That's space for all the variables.
+	entry inputs[52];
+	entry gatein[52];
+	entry gateout[52];
+	entry outputs[26];
+	//inputs size
+	int cursize;
+	//Space for our multiplexer inputs. Malloc and realloc as needed.
+	int *multiplex; 
+	//Number of inputs, number of outputs.
 	int inno;
 	int outno;
+	
+	int *invals;
+	int *outvals;
 	int *intemp;
 	int intemp_s;
 	int *outtemp;
 	int outtemp_s;
-	int i;
-	char *in;
-	char *out;
+	unsigned int i;
+	char in;
+	char out;
 	int decin;
 	int muxin;
 	int numin;
 	int numout;
-  
+	
 int main(int argc, char* argv[]){
 	if(argc != 3){
 		printf("Incorrect number of arguments.");
 	}
 	FILE cdf;
 	FILE ivf;
-	intemp = malloc(sizeof(int)*2);
-	intemp_s = 2;
-	outtemp = malloc(sizeof(int)*4);
-	outtemp_s = 4;
-	in = malloc(sizeof(char)*2);
-	out = malloc(sizeof(char)*4);
-	if(!(intemp&&outtemp&&in&&out)){
-		perror("mallocing error");
-		exit(1);
-	}
+	cursize = 0;
+	
 	if((cdf = fopen(argv[1], "r")) == NULL){
 		perror("Could not open Circuit Description File.");
 		exit(1);
@@ -53,15 +60,15 @@ int main(int argc, char* argv[]){
 				perror("Could not read inno");
 				exit(1);
 			}
-			inputs = malloc(inno * sizeof(char));
-			invals = malloc(inno * sizeof(int));
 			for(i = 0; i < inno; i++){
-				if(fscanf(cdf, "%c", inputs[i]) != 1){
+				if(fscanf(cdf, "%c", (inputs[i])->name) != 1){
 					perror("Could not read all input variables.");
 					exit(1);
 				}
 			}
-			/*Finished setting up all inputs variables and malloced space for their values.*/
+			cursize = inno;
+			/*Finished setting up all initial input variables.
+			Temporary ones will be set up as we go.*/
 		}
 		
 		else if(strcmp(buffer, "OUTPUTVAR") == 0){
@@ -69,22 +76,20 @@ int main(int argc, char* argv[]){
 				perror("Could not read outno");
 				exit(1);
 			}
-			outputs = malloc(outno * sizeof(char));
-			outvals = malloc(outno * sizeof(int));
 			for(i = 0; i < outno; i++){
-				if(fscanf(cdf, "%c", outputs[i]) != 1){
+				if(fscanf(cdf, "%c", (outputs[i]) ->name) != 1){
 					perror("Could not read all output variables.");
 					exit(1);
 				}
 			}
-			/*Finished setting up all output variables and malloced space for their values.*/
+			/*Finished setting up all output variables.*/
 		}
 	}
 	rewind(cdf);
 	/*Begin loading values.*/
 	while(fscanf(ivf, "%d", invals[0]) != EOF){
 		for(i = 1; i < inno; i++){
-			if(fscanf(ivf, "%d", invals[i]) != 1){
+			if(fscanf(ivf, "%d", (input[i]) -> value) != 1){
 				perror("Couldn't grab inputs values.");
 				exit(1);
 			}
@@ -95,19 +100,19 @@ int main(int argc, char* argv[]){
 				numin = 1;
 				numout = 1;
 				read(cdf);
-				outputs[outtemp[0]] = !inputs[intemp[0]];
+				(gateout[0]->value) = !(gatein[0]->value);
 			}
 			else if(strcmp(buffer, "AND") == 0){
 				numin = 2;
 				numout = 1;
 				read(cdf);
-				outputs[outtemp[0]] = (inputs[intemp[0]] && inputs[intemp[1]]);
+				(gateout[0]->value) = (gatein[0]->value) && (gatein[1]->value);
 			}
 			else if(strcmp(buffer, "OR") == 0){
 				numin = 2;
 				numout = 1;
 				read(cdf);
-				outputs[outtemp[0]] = (inputs[intemp[0]] || inputs[intemp[1]]);
+				(gateout[0]->value) = (gatein[0]->value) || (gatein[1]->value);
 			}
 			else if(strcmp(buffer, "DECODER") == 0){
 				if (fscanf(cdf, "%d", numin) != 1){
@@ -115,17 +120,11 @@ int main(int argc, char* argv[]){
 					exit(1);
 				}
 				numout = numin << 1 ;
-				if(numin > intemp_s){
-					realloc(intemp, sizeof(int)*numin);
-					realloc(in, sizeof(char)*numin);
+				read(cdf);
+				for(i = 0; i < numout; i++){
+					gateout[i]->value = 0;
 				}
-				if(numout > outtemp_s){
-					realloc(outtemp, sizeof(int)*numout);
-				}
-				
-				
-				read(cdf, decin, (i << 1));
-				
+				gateout[binary_to_gs_to_dec(gatesin, numin)]->value = 1;
 				
 			}
 			else if(strcmp(buffer, "MULTIPLEXER") == 0){
@@ -134,48 +133,55 @@ int main(int argc, char* argv[]){
 		}
 	}
 }
+/*It's definitely possible to go from binary to grey sequencing to decimal.*/
+int binary_to_dec(entry *gatesin, int numin){
+	int inpu = 0;
+	for(i = 0; i < numin; i++){
+		inpu << 1;
+		inpu += gatesin[i]->value;
+	}
+	for(i = 0; i < (numin << 1); i++){
+		
+	}
+}
 
-int find(char array[], char target, int saiz){
+entry find(entry array[], char target, int saiz){
 	int i;
 	for(i = 0; i < saiz; i++){
-		if(target == array[i]){
-			return i;
+		if(target == (array[i])->name){
+			return array[i];
 		}
 	}
-	return -1;
+	return NULL;
 }
-/*Reads variables and sets up values in intemp and outtemp. Use to perform gate operation in main.*/
-int read(FILE cdf, int intemp[], char in[], char *inputs, int outtemp[], char out[], char *outputs, int inno, int outno, int numin, int numout){
+/*Reads variables and sets up values in temporary, or output vars. Use to perform gate operation in main.*/
+int read(FILE cdf){
 	int a;
 	int b;
+	int c;
 	a = b = 0;
-	while(a <= numin){
-		if (fscanf(cdf, "%c", in[a]) != 1){
+	while(a < numin){
+		if (fscanf(cdf, "%c", in]) != 1){
 			perror("Could not read input");
 			exit(1);
 		}
-		a++;
-	}
-	while(b <= numout){
-		if(fscanf(cdf, "%c", out[b]) != 1){
-			perror("Could not read output");
-			exit(1);
-		}
-		b++;
-	}
-	a = 0;
-	b = 0;
-	while(a <= numin){
-		if((intemp[a] = find(inputs, in[a], inno)) == -1){
+		if((gatein[a] = find(inputs, in, inno)) == NULL){
 			perror("Could not find input");
 			exit(1);
 		}
 		a++;
 	}
-	while(b <= numout){
-		if((outtemp[b] = find(outputs, out[b], outno)) == -1){
-			perror("Could not find output");
+	while(b < numout){
+		if(fscanf(cdf, "%c", out) != 1){
+			perror("Could not read output");
 			exit(1);
+		}
+		if((gateout[b] = find(inputs, out, outno) == NULL){
+			if((gateout[b] = find(inputs, out, outno)) == NULL){
+				inputs[cursize] -> name = out;
+				gateout[b] = inputs[cursize];
+				cursize++;
+			}
 		}
 		b++;
 	}
